e0124d28b0ce769880dfaa76f56c6387cd1facd1
Simone Murri <simonemurri701@gmail.com>
2023-12-21 12:52:47 +0100

    preparazione vite-comics

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template lang="">
+    <div class="body">
+        <header>
+            <Header></Header>
+        </header>
+
+        <main>
+            <Main></Main>
+        </main>
+
+        <footer>
+            <Footer></Footer>
+        </footer>
+    </div>
+</template>
+<script>
+
+import Header from './components/header/Header.vue';
+import Main from './components/main/Main.vue';
+import Footer from './components/footer/Footer.vue';
+
+export default {
+  components: {
+    Header,
+    Main,
+    Footer,
+  }
+}
+</script>
+<style lang="">
+</style>
